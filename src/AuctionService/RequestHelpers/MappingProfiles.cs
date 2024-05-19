8fca914939b921da018b63ce6453b8c868c5aeb4
LukeAndrewHirst <luke.hirst.la@gmail.com>
2024-05-19 20:01:18 +0200

    Added rabbitMQ

--- src/AuctionService/RequestHelpers/MappingProfiles.cs
+++ src/AuctionService/RequestHelpers/MappingProfiles.cs
@@ -1,6 +1,7 @@
 using AuctionService.DTOs;
 using AuctionService.Entities;
 using AutoMapper;
+using Contracts;
 
 namespace AuctionService.RequestHelpers
 {
@@ -12,6 +13,9 @@ namespace AuctionService.RequestHelpers
             CreateMap<Item, AuctionDto>();
             CreateMap<CreateAuctionDto, Auction>().ForMember(i => i.Item, o => o.MapFrom(s => s));
             CreateMap<CreateAuctionDto, Item>();
+            CreateMap<AuctionDto, AuctionCreated>();
+            CreateMap<Auction, AuctionUpdated>().IncludeMembers(i => i.Item);
+            CreateMap<Item, AuctionUpdated>();
         }   
     }
 }
